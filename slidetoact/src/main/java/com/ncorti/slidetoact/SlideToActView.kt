180ca2e4de834786b847e5ab28afc5de443a619a
Tiago Araujo <taraujodesouza@gmail.com>
2019-10-06 20:32:38 +0200

    Migrated to AndroidX

--- slidetoact/src/main/java/com/ncorti/slidetoact/SlideToActView.kt
+++ slidetoact/src/main/java/com/ncorti/slidetoact/SlideToActView.kt
@@ -10,11 +10,11 @@ import android.graphics.*
 import android.graphics.drawable.AnimatedVectorDrawable
 import android.graphics.drawable.Drawable
 import android.os.Build
-import android.support.annotation.RequiresApi
-import android.support.graphics.drawable.AnimatedVectorDrawableCompat
-import android.support.graphics.drawable.VectorDrawableCompat
-import android.support.v4.content.ContextCompat
-import android.support.v4.widget.TextViewCompat
+import androidx.annotation.RequiresApi
+import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat
+import androidx.vectordrawable.graphics.drawable.VectorDrawableCompat
+import androidx.core.content.ContextCompat
+import androidx.core.widget.TextViewCompat
 import android.util.AttributeSet
 import android.util.TypedValue
 import android.util.Xml
