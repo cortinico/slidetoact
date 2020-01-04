906d3dd20623610aaca06e38f0581376aa50d721
Abhijeet Soni <abhijeet.nkt@gmail.com>
2020-01-04 17:44:54 +0530

    Added feature: bump vibration

--- example/src/main/java/com/ncorti/slidetoact/example/MainActivity.java
+++ example/src/main/java/com/ncorti/slidetoact/example/MainActivity.java
@@ -32,6 +32,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         findViewById(R.id.button_custom_icon).setOnClickListener(this);
         findViewById(R.id.button_reversed_slider).setOnClickListener(this);
         findViewById(R.id.button_animation_duration).setOnClickListener(this);
+        findViewById(R.id.button_bump_vibration).setOnClickListener(this);
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
