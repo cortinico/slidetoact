271fb61e98e26b430b41e64f452ce7cdffd56a9b
Nicola Corti <corti.nico@gmail.com>
2020-07-04 03:18:16 +0200

    Fix Ktlint

--- slidetoact/src/main/java/com/ncorti/slidetoact/SlideToActView.kt
+++ slidetoact/src/main/java/com/ncorti/slidetoact/SlideToActView.kt
@@ -21,7 +21,6 @@ import android.os.Vibrator
 import android.util.AttributeSet
 import android.util.Log
 import android.util.TypedValue
-import android.util.Xml
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewOutlineProvider
